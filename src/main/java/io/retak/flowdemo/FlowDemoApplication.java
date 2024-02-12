cae0a061e18ed31d24f98f79d7eef640286530d1
Ahmet Turgut Kati <hahahayesss@gmail.com>
2024-02-13 02:37:01 +0300

    init

---
+++ src/main/java/io/retak/flowdemo/FlowDemoApplication.java
@@ -0,0 +1,13 @@
+package io.retak.flowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/retak/flowdemo/FlowDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.retak.flowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
